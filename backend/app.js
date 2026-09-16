d80ed65474772626851cdaa28675eac2dae842e9
Renan Daboit <renan_daboit@estudante.sesisenai.org.br>
2026-09-16 11:46:51 -0300

    Nigeria 6

--- backend/app.js
+++ backend/app.js
@@ -55,6 +55,29 @@ server.get('/produtos/busca/:nome', (req, res) => {
     });
 });
 
+server.post('/produtos', (req, res) => {
+    const { nome, cor, textura, peso, unidade_medida, aplicacao, data_validade, estoque_minimo, estoque_atual, preco_unitario, id_categoria } = req.body;
+    const sql = 'INSERT INTO Produto (nome, cor, textura, peso, unidade_medida, aplicacao, data_validade, estoque_minimo, estoque_atual, preco_unitario, id_categoria) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+
+    if(nome == null || cor == null || textura == null || peso == null || unidade_medida == null || aplicacao == null || data_validade == null || estoque_minimo == null || estoque_atual == null || preco_unitario == null || id_categoria == null) {
+        return res.status(400).send({erro: 'Todos os campos são obrigatórios.'});
+    }
+
+
+    connection.query(sql, [nome, cor, textura, peso, unidade_medida, aplicacao, data_validade, 
+        estoque_minimo, estoque_atual, preco_unitario, id_categoria], (erro, resultado) => {
+        if (erro) {
+            return res.status(500).send({erro: erro.message});
+        }
+        res.status(201).json(resultado);
+        
+        res.json({
+            mensagem: 'Produto cadastrado com sucesso!',
+            id: resultado.insertId
+        })
+    });
+});
+
 const PORT = 3056;
 
 server.listen(PORT, () => {
